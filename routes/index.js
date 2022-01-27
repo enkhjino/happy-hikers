e9aa1e11298088174af382218780a19a4fdd5962
enkhjino <enkhjin.ochirbal1@gmail.com>
2022-01-26 23:16:38 -0800

    fix home page/trails

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '' });
+  res.render('index', { title: 'THE HAPPY HIKERS CLUB' });
 });
 
 // Google OAuth login route
